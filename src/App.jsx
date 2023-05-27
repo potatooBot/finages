ba8327f27a8906274555b492a8c13d6597933035
potatooBot <kumarkeshav825@gmail.com>
2023-05-27 20:39:04 +0530

    COntact Page Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero,Contact } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -21,17 +21,12 @@ const App = () => (
   
         <Stats />
         <Business />
-   <Footer/>
-        {/* <Contact/> */}
-        {/* <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-         */}
+<Contact/>
+
+       
       </div>
     </div>
+   
   </div>
 );
 

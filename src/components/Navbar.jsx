68c1bbdea68e93c4cfce02993ace2a0d3332a9da
potatooBot <kumarkeshav825@gmail.com>
2023-05-31 21:21:35 +0530

    Animations added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <div className="sm:hidden flex flex-1 justify-end items-center" style={{zIndex:1}}>
         <img
           src={toggle ? close : menu}
           alt="menu"
